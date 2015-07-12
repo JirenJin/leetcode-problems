4f7fcb6f526118c938ac34060b5644dad5e1bd75
Jiren Jin <jinjiren@foxmail.com>
2015-07-12 17:11:14 +0900

    modify max depth of binary tree

--- max_depth_binary_tree.py
+++ max_depth_binary_tree.py
@@ -8,13 +8,9 @@ class Solution:
     # @param {TreeNode} root
     # @return {integer}
     def maxDepth(self, root):
-        left_depth = 0
-        right_depth =0
         if root:
-            if root.left:
-                left_depth =  self.maxDepth(root.left)
-            if root.right:
-                right_depth = self.maxDepth(root.right)
+            left_depth =  self.maxDepth(root.left)
+            right_depth = self.maxDepth(root.right)
             return max(left_depth, right_depth) + 1
         else:
             return 0
